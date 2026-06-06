317268648fe9cedb7303ecd3f2b702bde2176dd8
Martinhocosta1999 <martinhocosta1999@gmail.com>
2026-06-06 17:39:05 -0300

    Task01
    Criação do formulario de Cadastro, Criação da Classe usuário com a função conecta.

---
+++ Pages/cadastro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2 class="titlo-page">CADASTRO DE USUARIO</h2>
+    <form method="post">
+        <input type="text" name="nome" id="" class="input-forms" placeholder="Digite seu nome.">
+        <input type="tel" name="telefone" id="" class="input-forms" placeholder="Digite seu telefone.">
+        <input type="email" name="email" id="" class="input-forms" placeholder="Digite seu email.">
+        <input type="password" name="senha" id="" class="input-forms" placeholder="Digite seu senha.">
+        <input type="password" name="confsenha" id="" class="input-forms" placeholder="Comfirme sua senha.">
+        <input type="submit" value="CADASTRAR">
+        <P>Já é Cadastrado? Clique <a href="login.php">Aqui</a> para Acessar.</P>
+
+    </form>
+
+</body>
+
+</html>
